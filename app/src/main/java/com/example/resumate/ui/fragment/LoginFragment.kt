22eefc535d2ff44120645bb47629b43f86f35936
DanaShkokani <shkokani.1@osu.edu>
2020-02-22 17:56:42 -0500

    Firebase still not working - changed some login functionalities

--- app/src/main/java/com/example/resumate/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/resumate/ui/fragment/LoginFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.resumate.R
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment(), View.OnClickListener{
     companion object {
@@ -18,19 +21,40 @@ class LoginFragment : Fragment(), View.OnClickListener{
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val firebaseAuth = FirebaseAuth.getInstance()
         val v = inflater.inflate(R.layout.login_layout, container, false)
-
+        val email: EditText = v.findViewById(R.id.email)
+        val password: EditText = v.findViewById(R.id.password)
         val loginButton: Button = v.findViewById(R.id.login_button)
-        loginButton.setOnClickListener(this)
         val signupButton: Button = v.findViewById(R.id.signup_button)
-        signupButton.setOnClickListener(this)
+        val emailId = email.text.toString()
+        val paswd = password.text.toString()
+
+        signupButton.setOnClickListener {
+            if(emailId.isEmpty() || paswd.isEmpty()) {
+                if (emailId.isEmpty()) {
+                    email.setError("Email was not provided")
+                    email.isFocusable = true
+                }
+                if (paswd.isEmpty()) {
+                    password.setError("Password was not provided")
+                    password.isFocusable = true
+                }
+            }
+                //firebaseAuth.createUserWithEmailAndPassword(emailId, paswd)
+        }
+        loginButton.setOnClickListener{
+            //firebaseAuth.signInWithEmailAndPassword(emailId, paswd)
+            loginButton.setOnClickListener(this)
+        }
 
         return v
     }
 
+
     override fun onClick(v: View) {
         when(v.id){
-            R.id.login_button -> goToOCR()
+                R.id.login_button -> goToOCR()
         }
     }
 
@@ -38,4 +62,6 @@ class LoginFragment : Fragment(), View.OnClickListener{
         activity?.finish()
         startActivity(Intent("com.example.resumate.ui.main.OCR"))
     }
+
+
 }
