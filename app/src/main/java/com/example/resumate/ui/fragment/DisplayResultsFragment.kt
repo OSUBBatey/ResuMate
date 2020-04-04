85a8a2e02d85685042e08d8f4e6ac06053b827b5
Brian Batey <batey.18@osu.edu>
2020-04-03 22:28:54 -0400

    Add RecyclerView to Resuls

--- app/src/main/java/com/example/resumate/ui/fragment/DisplayResultsFragment.kt
+++ app/src/main/java/com/example/resumate/ui/fragment/DisplayResultsFragment.kt
@@ -8,11 +8,15 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.resumate.R
+import com.example.resumate.ui.recycler.RecyclerItemObj
+import com.example.resumate.ui.recycler.ResListAdapter
 import com.example.resumate.utilities.DataModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.display_results_layout.*
 import org.w3c.dom.Text
 import kotlin.math.round
 
@@ -20,6 +24,8 @@ import kotlin.math.round
 class DisplayResultsFragment : Fragment(), View.OnClickListener{
     private lateinit var firebaseDatabase: DatabaseReference
     private var url = OCRFragment.job_url
+    private lateinit var mAdapter : ResListAdapter
+    private val matchList : ArrayList<RecyclerItemObj> = ArrayList()
 
     companion object {
         fun newInstance() = DisplayResultsFragment()
@@ -37,23 +43,27 @@ class DisplayResultsFragment : Fragment(), View.OnClickListener{
         val saveJobButton: Button = v.findViewById(R.id.save_job_button)
         val percentageText: TextView = v.findViewById(R.id.percentage_text)
 
-        var count = 0
         saveJobButton.setOnClickListener(this)
         doneButton.setOnClickListener(this)
 
-        // Count how many job skills match with the user skills and display them
-        for(i in DataModel.jobSkills){
-            if (DataModel.userSkills.contains(i)) {
-                println(i)
-                count++
-            }
-        }
+        initMatchList()
+
         // Calculate the percentage and round to two decimal places
-        val percentage = (count / (DataModel.jobSkills.size.toDouble())) * 100
+        val percentage = (matchList.size / (DataModel.jobSkills.size.toDouble())) * 100
         val percentage_rounded:Double = Math.round(percentage * 100.0) / 100.0
         percentageText.setText("Your resume matches " + percentage_rounded + "% of the needed skills for the job")
 
         return v
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        results_recycler.apply{
+            layoutManager = LinearLayoutManager(activity)
+            mAdapter =  ResListAdapter(matchList) { _, _ ->
+            }
+            adapter = mAdapter
+        }
+        super.onViewCreated(view, savedInstanceState)
     }
 
 
@@ -80,5 +90,12 @@ class DisplayResultsFragment : Fragment(), View.OnClickListener{
         }
     }
 
+    private fun initMatchList(){
+        for(element in DataModel.jobSkills){
+            if (DataModel.userSkills.contains(element)) {
+                matchList.add(RecyclerItemObj(R.drawable.ic_star, element))
+            }
+        }
+    }
 
 }
